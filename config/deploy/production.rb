e688033f1ae02ee6dab183015c3e62ea1cb5ac26
tackeyy <mailto.takita.yusuke@gmail.com>
2016-12-20 16:11:46 +0900

    Fix unicorn config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,10 +2,11 @@ set :stage, :production
 set :branch, 'master'
 set :rails_env, 'production'
 set :migration_role, 'db'
-set :linked_dirs, %w{log tmp/backup tmp/pids tmp/cache tmp/sockets vendor/bundle node_modules}
-set :workers, { '*' => 2 }
+set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle)
+set :linked_files, %w(.env config/database.yml config/secrets.yml)
+set :workers, '*': 2
 set :log_level, :debug
 
-role :app, %w{takita@160.16.221.72}
-role :web, %w{takita@160.16.221.72}
-role :db, %w{takita@160.16.221.72}
+role :app, %w(takita@160.16.221.72)
+role :web, %w(takita@160.16.221.72)
+role :db, %w(takita@160.16.221.72)
