d35822eca36b0071b55bdbfe516c6b1d289923dc
tackeyy <mailto.takita.yusuke@gmail.com>
2017-02-02 11:57:53 +0900

    Fix order and add .rubocop.yml

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @posts = Post.order(:created_at).limit(Post::LATEST_POSTS_COUNT).decorate
+    @posts = Post.order(created_at: :desc)
+      .limit(Post::LATEST_POSTS_COUNT).decorate
   end
 end
