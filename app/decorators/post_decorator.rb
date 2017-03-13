4ac89923e7edb41ea3d74489aec7d437a53be0eb
tackeyy <mailto.takita.yusuke@gmail.com>
2017-03-13 20:33:04 +0900

    Enable gist

--- app/decorators/post_decorator.rb
+++ app/decorators/post_decorator.rb
@@ -2,18 +2,20 @@ class PostDecorator < Draper::Decorator
   delegate_all
 
   OPTIONS = {
-    filter_html:     true,
+    # NOTE: gistを表示したいため以下のオプションを外す
+    # filter_html:     true,
     hard_wrap:       true,
     link_attributes: { rel: 'nofollow', target: '_blank' }
   }.freeze
 
   EXTENSIONS = {
     autolink:           true,
-    superscript:        true
+    superscript:        true,
+    tables:             true
   }.freeze
 
   def body_as_html
-    renderer = Redcarpet::Render::HTML.new(OPTIONS)
+    renderer = Redcarpet::Render::OriginalHTML.new(OPTIONS)
     markdown = Redcarpet::Markdown.new(renderer, EXTENSIONS)
 
     markdown.render(model.body).html_safe
